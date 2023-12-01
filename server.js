60531d7b7d3bee59f875a07a590270fae8a71687
midlaj <miduzack001@gmail.com>
2023-12-01 20:59:16 +0530

    submitworking

--- server.js
+++ server.js
@@ -1,23 +1,108 @@
+
  var http =require('http')
- var fs =require('fs')
- 
- http.createServer((req,response)=>{
+ var fs = require('fs')
+
+
+ http.createServer((req,res)=>{
+    var route = req.url
+    let method = req.method
+    
    
 
-   if(req.url==='/'){
-    let save = fs.readFileSync('index.html',"utf-8") //line by line check readfilesync
-    response.write(save)
-    response.end();
+   if(route === '/'){
+    let data = fs.readFileSync('index.html',"utf-8")    //line by line check readfilesync
+        try{
+          res.writeHead(200,{'Content-Type':'text/html'})
+          res.end(data);
+        } catch(err) {
+        
+          res.writeHead(500,{'Content-Type':'text/html'})
+          res.end("Error!!")
+        }
+   
+    
+   
+}else if(route.toLowerCase() ==='/form?'){
+     let data = fs.readFileSync('form.html',"utf-8")
+       try{
+        res.writeHead(200,{'content-Type':'text/html'})
+        res.end(data)
+      }catch(err){
+        res.writeHead(500,{'content-Type':'text/html'})
+        res.end("ERROR>>")
+      }
+    
+
+   
+}else if( route.toLocaleLowerCase() ==='/home?'){
+    let data= fs.readFileSync('index.html','utf-8')
+      try{
+        res.writeHead(200,{'content-Type':'text/html'})
+        res.end(data)
+      }catch(err){
+       
+        res.writeHead(500,{'content-Type':'text/html'})
+        res.end("ERROR IN The PAGE")
+    
+      }
+  
+}else if( route==='/submit' &&  method==="POST"){
+     
+  let body = ''
+     
+      req.on('data',(chunk)=>{
 
 
-   }else if(req.url==='/form?'){
-    let save1 = fs.readFileSync('form.html',"utf-8")
-    response.end(save1)
+        body = body + chunk
 
-   }else if(req.url==='/home?'){
-    let save3 = fs.readFileSync('index.html','utf-8')
-    response.end(save3)
+      });
+      req.on('end',()=>{
+
+        try{
+          let recevedatas= JSON.parse(body)
+          console.log(recevedatas);
+
+        fs.readFile('node.json','utf-8',(err,data)=>{
+          if(err){
+            res.writeHead(500,{'content-Type':'text/html'})
+            res.end("ERROR IN The PAGE")
+        
+          }else{
+            const stringdata = [];
+            stringdata =JSON.parse(data)
+            const newarray = stringdata.length + 1
+
+            recevedatas.id = newarray
+            stringdata.push(recevedatas) //PUSHED NEW USER FILE TO SAME ARRAY as object
+ 
+          }
+        })
+
+        }catch(err){
+
+          res.writeHead(400,{'content-Type':'text/plain'})
+          res.end('ERROR')
+
+        }
+          
+      })
    }
+   else{
+    res.writeHead(404,{'content-Type':'text/plain'})
+    res.end("NOT FOUND")
+   }
+  //  else if(req.url==='/ok'){
+  //   res.end(fs.readFileSync('./node.json',"utf-8"))
+  //  }
 
    
  }).listen(2000)    
+
+
+
+
+
+
+
+
+ 

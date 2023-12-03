0fc10dbe4fa4d6a4cfc958d1fb0d3ab39eca007d
midlaj <miduzack001@gmail.com>
2023-12-03 19:43:40 +0530

    smallchanges

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
  
-  function addUser() {
-    // Dummy data for demonstration
-    const userData = {
-      name: "John Doe",
-      phoneNumber: "123-456-7890",
-      age: 25,
-      gmail: "john.doe@gmail.com"
-    };
+  // function addUser() {
+  //   // Dummy data for demonstration
+  //   const userData = {
+  //     name: "John Doe",
+  //     phoneNumber: "123-456-7890",
+  //     age: 25,
+  //     gmail: "john.doe@gmail.com"
+  //   };
 
-    // Create a new table row
-    const newRow = document.createElement("tr");
+  //   // Create a new table row
+  //   const newRow = document.createElement("tr");
 
-    // Add data cells
-    newRow.innerHTML = `
-      <td>${userData.name}</td>
-      <td>${userData.phoneNumber}</td>
-      <td>${userData.age}</td>
-      <td>${userData.gmail}</td>
-    `;
+  //   // Add data cells
+  //   newRow.innerHTML = `
+  //     <td>${userData.name}</td>
+  //     <td>${userData.phoneNumber}</td>
+  //     <td>${userData.age}</td>
+  //     <td>${userData.gmail}</td>
+  //   `;
 
-    // Append the new row to the table body
-    document.getElementById("userTableBody").appendChild(newRow);
-  }
+  //   // Append the new row to the table body
+  //   document.getElementById("userTableBody").appendChild(newRow);
+  // }
  
--- server.js
+++ server.js
@@ -1,184 +1,183 @@
 
- var http =require('http')
- var fs = require('fs')
+var http =require('http')
+var fs = require('fs')
 const { parse } = require('path')
 
 
- http.createServer((req,res)=>{
-    var route = req.url
-    let method = req.method
-    
+http.createServer((req,res)=>{
+   var route = req.url
+   let method = req.method
    
-
-   if(route === '/'){
-    let data = fs.readFileSync('index.html',"utf-8")    //line by line check readfilesync
-        try{
-          res.writeHead(200,{'Content-Type':'text/html'})
-          res.end(data);
-        } catch(err) {
-        
-          res.writeHead(500,{'Content-Type':'text/html'})
-          res.end("Error!!")
-        }
-   
-    
-   
-}else if(route.toLowerCase() ==='/form?'){
-     let data = fs.readFileSync('form.html',"utf-8")
-       try{
-        res.writeHead(200,{'Content-Type':'text/html'})
-        res.end(data)
-      }catch(err){
-        res.writeHead(500,{'Content-Type':'text/html'})
-        res.end("ERROR>>")
-      }
-    
-
-   
-}else if( route.toLocaleLowerCase() ==='/home?'){
-    let data= fs.readFileSync('index.html','utf-8')
-      try{
-        res.writeHead(200,{'Content-Type':'text/html'})
-        res.end(data)
-      }catch(err){
-       
-        res.writeHead(500,{'Content-Type':'text/html'})
-        res.end("ERROR IN The PAGE")
-    
-      }
   
+
+  if(route === '/'){
+   let data = fs.readFileSync('index.html',"utf-8")    //line by line check readfilesync
+       try{
+         res.writeHead(200,{'Content-Type':'text/html'})
+         res.end(data);
+       } catch(err) {
+       
+         res.writeHead(500,{'Content-Type':'text/html'})
+         res.end("Error!!")
+       }
+  
+   
+  
+}else if(route.toLowerCase() ==='/form?'){
+    let data = fs.readFileSync('form.html',"utf-8")
+      try{
+       res.writeHead(200,{'Content-Type':'text/html'})
+       res.end(data)
+     }catch(err){
+       res.writeHead(500,{'Content-Type':'text/html'})
+       res.end("ERROR>>")
+     }
+   
+
+  
+}else if( route.toLocaleLowerCase() ==='/home?'){
+   let data= fs.readFileSync('index.html','utf-8')
+     try{
+       res.writeHead(200,{'Content-Type':'text/html'})
+       res.end(data)
+     }catch(err){
+      
+       res.writeHead(500,{'Content-Type':'text/html'})
+       res.end("ERROR IN The PAGE")
+   
+     }
+ 
 }else if( route==='/submit' &&  method==="POST"){
-     
-  let body = ''
-     
-      req.on('data',(chunk)=>{
+    
+ let body = ''
+    
+     req.on('data',(chunk)=>{
 
 
-        body = body + chunk
+       body = body + chunk
 
-      });
-      req.on('end',()=>{
+     });
+     req.on('end',()=>{
 
-        try{
-          let recevedatas= JSON.parse(body)
+       try{
+         let recevedatas= JSON.parse(body)
 
-          console.log(recevedatas);
+         console.log(recevedatas);
 
-        fs.readFile('node.json','utf-8',(err,data)=>{
-          if(err){
-            res.writeHead(500,{'Content-Type':'text/html'})
-            res.end("ERROR IN The PAGE")
-        
-          }else{
-            var stringdata = [];
-            stringdata =JSON.parse(data)
-            var newarray = stringdata.length + 1
+       fs.readFile('node.json','utf-8',(err,data)=>{
+         if(err){
+           res.writeHead(500,{'Content-Type':'text/html'})
+           res.end("ERROR IN The PAGE")
+       
+         }else{
+           var stringdata = [];
+           stringdata =JSON.parse(data)
+           var newarray = stringdata.length + 1
 
-            recevedatas.id = newarray
-            stringdata.push(recevedatas) //push new user file to same array as object
- 
+           recevedatas.id = newarray
+           stringdata.push(recevedatas) //push new user file to same array as object
 
-            fs.writeFile("node.json", JSON.stringify(stringdata, null, 2), 'utf-8', (err) => {
-              if (err) {
-                  res.writeHead(500, { 'Content-Type': 'text/html' });
-                  res.end(err);
-              } else {
-                  res.writeHead(200, { 'Content-Type': 'text/html' });
-                  res.end("DATA saved ");
-              }
-          });
-          
 
-            
-          }
-        })
+           fs.writeFile("node.json", JSON.stringify(stringdata, null, 2), 'utf-8', (err) => {
+             if (err) {
+                 res.writeHead(500, { 'Content-Type': 'text/html' });
+                 res.end(err);
+             } else {
+                 res.writeHead(200, { 'Content-Type': 'text/html' });
+                 res.end("DATA saved ");
+             }
+         });
+         
 
-        }catch(err){
+           
+         }
+       })
 
-          res.writeHead(400,{'Content-Type':'text/plain'})
-          res.end('ERROR'+err)
+       }catch(err){
 
-        }
-          
-      });
+         res.writeHead(400,{'Content-Type':'text/plain'})
+         res.end('ERROR'+err)
+
+       }
+         
+     });
 }else if(route.toLocaleLowerCase() === '/getdetails' && method === 'GET'){
-        fs.readFile("node.json","utf-8",(err,data)=>{
-          if(err){
-            res.writeHead(400,{'Content-Type':'text/html'})
-            res.end(err)
+       fs.readFile("node.json","utf-8",(err,data)=>{
+         if(err){
+           res.writeHead(400,{'Content-Type':'text/html'})
+           res.end(err)
 
-          }else{
-            res.writeHead(200,{'Content-Type':'text/html'})
-            res.end(data)
-          }
+         }else{
+           res.writeHead(200,{'Content-Type':'text/html'})
+           res.end(data)
+         }
 
-        });
- 
+       });
+
 }else if(route.toLocaleLowerCase ==="/editdetails" && method==="PUT" ){
 
-  const prsurl=parse.url(route.true);
-  const userid =parseInt(prsurl.pathname.split('/').pop());
+ const prsurl=parse.url(route.true);
+ const userid =parseInt(prsurl.pathname.split('/').pop());
 
 
-  let body = '';
-  req.on("data",(chunk)=>{
-    body = body + chunk
-  });
+ let body = '';
+ req.on("data",(chunk)=>{
+   body = body + chunk
+ });
 
-  req.on("data",()=>{
-    try{
-      const modifaid = JSON.parse(body)
+ req.on("data",()=>{
+   try{
+     const modifaid = JSON.parse(body)
 
-      fs.readFile('node.json',"utf-8",(err,data)=>{
-        if(err){
-          res.writeHead(500,{'Content-Type':'text/html'})
-            res.end("ERROR IN The PAGE")
-        
-        }else{
-          let modi = JSON.parse(data)
+     fs.readFile('node.json',"utf-8",(err,data)=>{
+       if(err){
+         res.writeHead(500,{'Content-Type':'text/html'})
+           res.end("ERROR IN The PAGE")
+       
+       }else{
+         let modi = JSON.parse(data)
 
-          for(let i = 0;i <data.length ; i++){
-            if(modi[i].id===userid){
-              modi[i]=modifaid;
-              break;
-            }
+         for(let i = 0;i <data.length ; i++){
+           if(modi[i].id===userid){
+             modi[i]=modifaid;
+             break;
+           }
 
-          
+         
 
-            fs.writeFile("node.json", JSON.stringify(modi, null, 2), 'utf-8', (err) => {
-              if (err) {
-                  res.writeHead(500, { 'Content-Type': 'text/html' });
-                  res.end(err);
-              } else {
-                  res.writeHead(200, { 'Content-Type': 'text/html' });
-                  res.end("DATA saved ");
-              }
-          });
-          }
-        }
-      })
-    }catch{
+           fs.writeFile("node.json", JSON.stringify(modi, null, 2), 'utf-8', (err) => {
+             if (err) {
+                 res.writeHead(500, { 'Content-Type': 'text/html' });
+                 res.end(err);
+             } else {
+                 res.writeHead(200, { 'Content-Type': 'text/html' });
+                 res.end("DATA saved ");
+             }
+         });
+         }
+       }
+     })
+   }catch{
 
-    }
-  })
+   }
+ })
 
 }
-   else{
-    res.writeHead(404,{'content-Type':'text/plain'})
-    res.end("NOT FOUND")
-   }
-  //  else if(req.url==='/ok'){
-  //   res.end(fs.readFileSync('./node.json',"utf-8"))
-  //  }
+  else{
+   res.writeHead(404,{'content-Type':'text/plain'})
+   res.end("NOT FOUND")
+  }
+ //  else if(req.url==='/ok'){
+ //   res.end(fs.readFileSync('./node.json',"utf-8"))
+ //  }
 
-   
- }).listen(2000)    
-
-
+  
+}).listen(2000)    
 
 
 
 
 
 
- 
+
+
